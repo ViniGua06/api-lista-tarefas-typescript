ce7d0c87c249bbc23617f3a637e2a0ce52691f11
Vinicius Guarnieri <viniguarnierisouza@gmail.com>
2023-12-03 21:41:08 -0300

    2.1

--- dist/db.js
+++ dist/db.js
@@ -1,7 +1,7 @@
 "use strict";
 const mysql = require("mysql");
 const db = mysql.createConnection(process.env.DATABASE_URL ||
-    'mysql://nwqeoav2uvwn01t36g1a:pscale_pw_ht9apa46MNsH4S7j3UPpGa2EbbqJQyhoClXKN6sT21X@aws.connect.psdb.cloud/db_2?ssl={"rejectUnauthorized":true}');
+    'mysql://qojfwreq7oqhykdp8c6y:pscale_pw_PyPIW0lBXMiCBtnqvGkAvnAAQ1wjZRHFZVo8fvf3Xkl@aws.connect.psdb.cloud/db_2?ssl={"rejectUnauthorized":true}');
 db.connect((error) => {
     if (error) {
         console.log(error);
@@ -9,31 +9,14 @@ db.connect((error) => {
     }
     console.log("Servidor rodando");
 });
-const tables = `create table if not exists tarefa(
-	id int unsigned not null auto_increment,
-    titulo varchar(20) not null,
-    descricao varchar(30) not null,
-    prioridade char(1) not null,
-    usuario_id int unsigned not null,
-    imagem_capa text,
-    primary key (id)
-) engine = innoDb;`;
-db.query(tables, (error) => {
+const tables = `select * from usuario`;
+db.query(tables, (error, result) => {
     if (error) {
         console.log(error);
         return;
     }
-    console.log("Tabela criada!");
-});
-const script2 = `GRANT ALL PRIVILEGES ON *.* TO 'nwqeoav2uvwn01t36g1a'@'%' IDENTIFIED BY 'Mel060616' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-`;
-db.query(script2, (error) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    console.log("privas");
+    console.log(result);
 });
 //
 module.exports = db;
+//pscale_pw_PyPIW0lBXMiCBtnqvGkAvnAAQ1wjZRHFZVo8fvf3Xkl
--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 const db = mysql.createConnection(
   process.env.DATABASE_URL ||
-    'mysql://nwqeoav2uvwn01t36g1a:pscale_pw_ht9apa46MNsH4S7j3UPpGa2EbbqJQyhoClXKN6sT21X@aws.connect.psdb.cloud/db_2?ssl={"rejectUnauthorized":true}'
+    'mysql://qojfwreq7oqhykdp8c6y:pscale_pw_PyPIW0lBXMiCBtnqvGkAvnAAQ1wjZRHFZVo8fvf3Xkl@aws.connect.psdb.cloud/db_2?ssl={"rejectUnauthorized":true}'
 );
 
 db.connect((error: any) => {
@@ -14,37 +14,18 @@ db.connect((error: any) => {
   console.log("Servidor rodando");
 });
 
-const tables = `create table if not exists tarefa(
-	id int unsigned not null auto_increment,
-    titulo varchar(20) not null,
-    descricao varchar(30) not null,
-    prioridade char(1) not null,
-    usuario_id int unsigned not null,
-    imagem_capa text,
-    primary key (id)
-) engine = innoDb;`;
+const tables = `select * from usuario`;
 
-db.query(tables, (error: any) => {
+db.query(tables, (error: any, result: any | any[]) => {
   if (error) {
     console.log(error);
     return;
   }
 
-  console.log("Tabela criada!");
-});
-
-const script2 = `GRANT ALL PRIVILEGES ON *.* TO 'nwqeoav2uvwn01t36g1a'@'%' IDENTIFIED BY 'Mel060616' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-`;
-
-db.query(script2, (error: any) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  console.log("privas");
+  console.log(result);
 });
 
 //
 module.exports = db;
+
+//pscale_pw_PyPIW0lBXMiCBtnqvGkAvnAAQ1wjZRHFZVo8fvf3Xkl
