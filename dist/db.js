b152e7464e54ba1063f15a7dc46049a1f7432f31
Vinicius Guarnieri <viniguarnierisouza@gmail.com>
2023-12-03 21:06:28 -0300

    1.7

--- dist/db.js
+++ dist/db.js
@@ -1,19 +1,29 @@
 "use strict";
-const mysql = require("mysql");
-const db = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-});
-db.getConnection((error, connection) => {
+const mysql = require("mysql2");
+const db = mysql.createConnection(process.env.DATABASE_URL ||
+    'mysql://nwqeoav2uvwn01t36g1a:pscale_pw_ht9apa46MNsH4S7j3UPpGa2EbbqJQyhoClXKN6sT21X@aws.connect.psdb.cloud/db_2?ssl={"rejectUnauthorized":true}');
+db.connect((error) => {
     if (error) {
-        console.error("Erro ao conectar ao banco de dados:", error);
+        console.log(error);
         return;
     }
-    console.log("Conexão bem-sucedida.");
-    connection.release();
+    console.log("Servidor rodando");
+});
+const tables = `create table if not exists tarefa(
+	id int unsigned not null auto_increment,
+    titulo varchar(20) not null,
+    descricao varchar(30) not null,
+    prioridade char(1) not null,
+    usuario_id int unsigned not null,
+    imagem_capa text,
+    primary key (id)
+) engine = innoDb;`;
+db.query(tables, (error) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log("Tabela criada!");
 });
 //
 module.exports = db;
