bfe862d40ac91616be15589b25ba5c123340d788
Vinicius Guarnieri <viniguarnierisouza@gmail.com>
2023-12-04 17:32:14 -0300

    agora vai

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,6 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection(
-  process.env.DATABASE_URL ||
-    'mysql://0vfqord8ecqcisd2wgid:pscale_pw_HIj6J2Er3WvhA5SKkhxuDTJi0qpbMBMlSciW6aDTe6P@aws.connect.psdb.cloud/db_2?ssl={"rejectUnauthorized":true}'
-);
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((error: any) => {
   if (error) {
